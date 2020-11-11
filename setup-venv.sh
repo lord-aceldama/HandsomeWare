591097e771253cb716fb24d532e45405d169e0a9
lord-aceldama <aceldama.v1.0@gmail.com>
2020-11-11 03:49:07 +0000

    scripts & docs

--- setup-venv.sh
+++ setup-venv.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Creating virtual environment..."
 python3 -m venv venv
+
+echo "Installing pip(s)..."
 . ./venv/bin/activate
 pip3 install -r requirements.txt
 deactivate
 
+echo "Making scripts executable..."
 chmod +x *.py
 chmod +x *.sh
+
+echo "All done :)"
